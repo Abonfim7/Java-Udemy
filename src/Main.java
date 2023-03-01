bcef755cf23f9464a495be22e6370714ecd35e6d
Andrea <andrearb738@gmail.com>
2023-02-28 21:36:22 -0300

    Curso Java_Udemy Seção 4.23 Delimitando casas decimais

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main{
     public static void main (String[] args){
 
-        int y = 32;
         double x = 10.35784;
-        System.out.println(y);
         System.out.println(x);
-        System.out.println("Bom dia!");
+        System.out.printf("%.2f%n", x);
 
+
+        //%2f = delimitando 2 casas decimais
+        //%n (ou \n) = quebra de linha
     }
 }
